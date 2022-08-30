73fdbdeac8f82c42c34324a2dd8dbb12cb2a7ad3
GabrielOJorge <goliver331@protonmail.com>
2022-08-30 16:53:47 -0300

    create index.html and index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+class LinkedList {
+  constructor(list) {
+
+  };
+
+  append(value) {
+
+  };
+
+  prepend(value) {
+
+  };
+
+  size() {
+
+  };
+
+  head() {
+
+  };
+
+  tail() {
+
+  };
+
+  at(index) {
+
+  };
+
+  pop() {
+
+  };
+
+  contains(value) {
+
+  };
+
+  find(value) {
+
+  };
+
+  toString() {
+
+  };
+};
+
+class Node {
+  value() {
+    return null;
+  };
+
+  nextNode = null;
+};
